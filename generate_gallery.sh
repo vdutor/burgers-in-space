816d806b67f1496bd99be8e6688debac740dad31
vdutor <dutordoirv@gmail.com>
2016-09-13 12:19:37 +0000

    progress on generate gallery script

--- generate_gallery.sh
+++ generate_gallery.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 for filename in *.{jpg,JPG,mp4,MP4}; do
       echo "
-      <li class=\"col-xs-6 col-sm-4 col-md-3\" data-responsive=\"$PWD/$filename 800\" data-src=\"$PWD/$filename\" data-sub-html=\"\">
-        <a href=\"\">
-            <img class=\"img-responsive\" src=\"$PWD/$filename\">
-        </a>
-      </li>"
+      <a href=\"$PWD/$filename\">
+        <img class=\"img-responsive\" src=\"$PWD/thumbnail/$filename\">
+      </a>"
 done
