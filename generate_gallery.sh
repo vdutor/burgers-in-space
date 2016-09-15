cc757e91a4df8967fa9a9acb094140c9ce70b37a
vdutor <dutordoirv@gmail.com>
2016-09-15 03:42:42 +0000

    fix generate gallery script

--- generate_gallery.sh
+++ generate_gallery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-serverroot = ${PWD#/usr/share/www}
+SVRROOT=${PWD#/usr/share/www}
 
 echo "<!-- Hidden video div -->"
 for filename in *.{mp4,MP4}; do
@@ -7,7 +7,7 @@ for filename in *.{mp4,MP4}; do
   echo "
   <div style=\"display:none;\" id=\"$noextension\">
     <video class=\"lg-video-object lg-html5\" controls preload=\"none\">
-      <source src=\"$serverroot/$filename\" type=\"video/mp4\">
+      <source src=\"$SVRROOT/$filename\" type=\"video/mp4\">
       Your browser does not support HTML5 video.
     </video>
   </div>"
@@ -18,18 +18,18 @@ echo "  <div class=\"demo-gallery\">"
 echo "    <ul id=\"lightgallery\" class=\"list-unstyled row\">"
 for filename in *.{jpg,JPG}; do
   echo "
-  <li class=\"col-xs-6 col-sm-4 col-md-3 li-gallery\" data-responsive=\"$serverroot/$filename 800\" data-src=\"$serverroot/$filename\" data-sub-html=\"\">
+  <li class=\"col-xs-6 col-sm-4 col-md-3 li-gallery\" data-responsive=\"$SVRROOT/$filename 800\" data-src=\"$SVRROOT/$filename\" data-sub-html=\"\">
     <a href=\"\">
-      <img class=\"img-gallery img-responsive\" src=\"$serverroot/thumbs/$filename\">
+      <img class=\"img-gallery img-responsive\" src=\"$SVRROOT/thumbs/$filename\">
     </a>
   </li>"
 done
 for filename in *.{mp4,MP4}; do
   noextension="${filename%.*}"
   echo "
-  <li class=\"col-xs-6 col-sm-4 col-md-3 li-gallery\" data-poster=\"$serverroot/thumbs/$noextension.jpg\" data-sub-html=\"\" data-html=\"#$noextension\" >
+  <li class=\"col-xs-6 col-sm-4 col-md-3 li-gallery\" data-poster=\"$SVRROOT/thumbs/$noextension.jpg\" data-sub-html=\"\" data-html=\"#$noextension\" >
     <a href=\"\">
-      <img class=\"img-gallery img-responsive\" src=\"$serverroot/thumbs/$noextension.jpg\" />
+      <img class=\"img-gallery img-responsive\" src=\"$SVRROOT/thumbs/$noextension.jpg\" />
     </a>
   </li>"
 done
